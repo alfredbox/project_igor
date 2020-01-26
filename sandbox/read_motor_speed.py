ca0efa80ca8d12a843cee0e575ce018190d6a796
alfredbox <alfred.y.box@gmail.com>
2020-01-26 04:03:46 +0000

    Code that does simple prints of input pin being pulled hi/lo

--- sandbox/read_motor_speed.py
+++ sandbox/read_motor_speed.py
@@ -2,3 +2,23 @@
 A simple program to read in the state of a motot encoder
 pin and compute and print the speed in RPM.
 '''
+from gpiozero import DigitalInputDevice
+#from signal import pause
+
+def on_change():
+   print('Change detected')
+
+def setup():
+    encoder_a = DigitalInputDevice(12)
+    while True:
+        encoder_a.wait_for_active()
+        print('ON!')
+        encoder_a.wait_for_inactive()
+        print('OFF!')
+    #encoder_a.when_activated = on_change
+    #encoder_a.when_deactivated = on_change
+
+if __name__ == "__main__":
+    print('Testing motor speed')
+    setup()
+    #pause()
