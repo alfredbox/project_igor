eba84e1a98f611f63fde5b814952005c8dc303ec
simbox <simon@aurora.tech>
2020-03-08 09:33:09 -0700

    Add asynchronous executor framework.

---
+++ state.py
@@ -0,0 +1,28 @@
+import collections
+
+class EncoderState:
+    def __init__(self):
+        self.activation_history = collections.deque(maxlen=4)
+
+class MotorState:
+    def __init__(self):
+        self.direction = True # Forward
+        self.rpm = 0
+        self.throttle = 0
+        # Raw state data
+        self.encoder_a = EncoderState()
+        self.encoder_b = EncoderState()
+
+class DriveState:
+    def __init__(self):
+        self.port_motor = MotorState()
+        self.sbrd_motor = MotorState()
+
+class IMUState:
+    def __init__(self):
+        self.gravity = (0., 0., 0.)
+
+class State:
+    def __init__(self):
+        self.imu_state = IMUState()
+        self.drive_state = DriveState() 
