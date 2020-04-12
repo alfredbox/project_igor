42774c23da2c54440d729cdd62263ef5f500e6cc
SimonBox <srb2242 [at] gmail.com>
2020-04-12 09:42:49 -0700

    Better plotting

--- tools/process_log.py
+++ tools/process_log.py
@@ -12,20 +12,26 @@ def sample_freq(data):
             durations.append(t - last)
         last = t
     times.pop()
+    plt.figure(1)
     plt.plot(times, durations)
-    plt.show()
+    plt.xlabel('Time s')
+    plt.ylabel('dt s')
+
 
 def throttle_angle_trace(data): 
     times = [t['timestamp'] for t in data]
     throttle = [t['set_throttle'] for t in data]
     angle = [t['control_angle'] for t in data]
+    plt.figure(2)
     plt.plot(times, throttle, times, angle)
-    plt.show()
+    plt.xlabel('Time s')
+    plt.legend(['Throttle', 'Angle degs'])
 
 
 def make_plots(data):
     sample_freq(data)
     throttle_angle_trace(data)
+    plt.show()
     
 
 def process(filename):
@@ -43,9 +49,10 @@ def main():
     parser = argparse.ArgumentParser(description='Process a log file created by Igor')
     parser.add_argument('-l',
                         '--log',
-                        required=True,
+                        default=None,
                         help='Path to the log file to be processed')
     args = parser.parse_args()
+    log = args.log if args.log is not None else latest_log()
     process(args.log)
 
 if __name__ == "__main__":
