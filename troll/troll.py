7cffd5d4769a37d1b8f748d6403214491d3b71e9
SimonBox <srb2242 [at] gmail.com>
2020-04-26 22:23:55 -0700

    Adding simple troll with bluetooth test

---
+++ troll/troll.py
@@ -0,0 +1,25 @@
+import json
+import socket
+import time
+
+SERVER_MAC = 'B8:27:EB:87:5B:87'
+PORT = 3
+BACKLOG = 1
+SIZE = 1024
+
+s = socket.socket(socket.AF_BLUETOOTH, 
+                  socket.SOCK_STREAM, 
+                  socket.BTPROTO_RFCOMM)
+for i in range(300):
+    try:
+        s.connect((SERVER_MAC, PORT))
+        break
+    except:
+        time.sleep(0.11111111)
+
+text = "Hello Igor"
+s.send(bytes(text, 'UTF-8'))
+data = s.recv(SIZE)
+print(data)
+s.close()
+
